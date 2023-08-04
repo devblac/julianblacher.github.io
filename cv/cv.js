19b6e6dffe6eaf46915cf278727b2ebed3f8b4de
Julian Blacher <julian.blacher@gmail.com>
2023-08-04 12:20:12 -0300

    updated EN cv

---
+++ cv/cv.js
@@ -0,0 +1,308 @@
+const cv = {
+  "longCV": false,
+  "month_names": ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
+  "headline": {
+    "name": "Julian Isaac Blacher",
+    "short": "Senior Software Engineer",
+    "experience_start": "2014-03-01",
+    "languages": [
+      { "id": 1, "name": "English", "image": "images/gb.svg", "level": "(fluent)", "color": "white" },
+      { "id": 2, "name": "Spanish", "image": "images/es.svg", "level": "(native)", "color": "white" }
+    ],
+    "links": [
+      {
+        "link": "julian.blacher@gmail.com",
+        "type": "Email"
+      },
+      {
+        "link": "https://github.com/devblac",
+        "type": "Github"
+      },
+      {
+        "link": "http://julianblacher.github.io/",
+        "type": "Blog"
+      },
+      {
+        "link": "https://twitter.com/julianblacher",
+        "type": "Twitter"
+      }
+    ],
+    "current_location": { "text": "Buenos Aires, Argentina", "image": "images/ar.svg", "color": "white" },
+    "born_location": { "text": "Buenos Aires, Argentina", "image": "images/ar.svg", "color": "white" },
+    "expertise_bullets": [
+      { "id": 1, "text": "Javascript", "color": "light_blue", "image": "images/javascript.svg" },
+      { "id": 1, "text": "React", "color": "light_blue", "image": "images/react.png" },
+      { "id": 2, "text": "Python", "color": "red", "image": "images/python.svg" },
+      { "id": 3, "text": "Databases", "color": "blue", "image": "images/sql.png" },
+      { "id": 4, "text": "Nodejs", "color": "blue", "image": "images/node.png" },
+      { "id": 5, "text": "AWS", "color": "yellow", "image": "images/aws.svg" },
+      { "id": 6, "text": "Kubernetes", "color": "blue", "image": "images/kubernetes.svg" },
+      { "id": 7, "text": "Microservices", "color": "white", "image": "images/microservices.png" },
+      { "id": 8, "text": "Cloud", "color": "blue", "image": "images/kafka.svg" },
+    ],
+    "interest_bullets": [
+      { "id": 1, "text": "Blockchain", "color": "white", "image": "" },
+      { "id": 2, "text": "Finance", "color": "white", "image": "" },
+      { "id": 3, "text": "AI", "color": "white", "image": "" }
+    ]
+  },
+  "open_source_projects": [
+    {
+      "name": "Portfolio tracker",
+      "short": "Python-JS crypto portfolio",
+      "github": "https://github.com/devblac/portfolio-tracker",
+      "blogpost": "",
+      "bullets": [
+        "Track your coins, metrics, historical value, all without storing any data",
+      ],
+      "stack": [
+        { "id": 1, "text": "Javascript", "color": "light_blue", "image": "images/javascript.svg" },
+        { "id": 2, "text": "Python", "color": "green", "image": "images/python.svg" }
+      ],
+      "special": true
+    },
+    {
+      "name": "Gym metrics",
+      "short": "Python-JS metrics for gym workouts",
+      "github": "https://github.com/devblac/gym-metrics",
+      "blogpost": "",
+      "bullets": [
+        "Register your training goals, with metrics and charts",
+      ],
+      "stack": [
+        { "id": 1, "text": "Javascript", "color": "light_blue", "image": "images/javascript.svg" },
+        { "id": 2, "text": "Python", "color": "green", "image": "images/python.svg" },
+        { "id": 2, "text": "AG Grid", "color": "green", "image": "images/aggrid.png" }
+      ],
+      "special": true
+    }
+  ],
+  "work_experience": [
+    {
+      "type": "work_experience",
+      "role": "Associate Software Engineer",
+      "company": "JP Morgan Chase",
+      "country": "images/usa.svg.png",
+      "company_link": "https://www.jpmorganchase.com/",
+      "started": "2018-09",
+      "ended": "Present",
+      "bullets": [
+        "2023: Currently leading development of a prediction tool using Bloomberg with hist data for quants and traders",
+        "2022: Led a team of 3 which developed a zero-configuration framework that improved stakeholder workflow efficiency by factor of 4",
+        "2021: Migrated from legacy product with zero downtime",
+        "2020: Led full implementation from scratch: team of 4 people",
+        "2019: Partnered with several teams and optimized the workflow of delivery of new features "
+      ],
+      "special": true,
+      "stack": [
+        { "id": 1, "text": "Javascript", "color": "red", "image": "images/javascript.svg" },
+        { "id": 2, "text": "Python", "color": "red", "image": "images/python.svg" },
+        { "id": 1, "text": "React", "color": "red", "image": "images/react.png" },
+        { "id": 4, "text": "Node.js", "color": "red", "image": "images/typescript.png" },
+        { "id": 5, "text": "AWS", "color": "yellow", "image": "images/aws.svg" },
+        { "id": 6, "text": "Jenkins", "color": "yellow", "image": "images/jenkins.svg.png" },
+        { "id": 7, "text": "Hydra", "color": "yellow", "image": "images/hydra.svg" },
+        { "id": 8, "text": "Redis", "color": "yellow", "image": "images/redis.svg" }
+      ]
+    },
+    {
+      "type": "work_experience",
+      "role": "Senior Software Developer",
+      "company": "Lumina Americas",
+      "country": "images/mex.svg.png",
+      "company_link": "https://www.luminaamericas.com/",
+      "started": "2018-09",
+      "ended": "2019-11",
+      "bullets": [
+        "MDDB (Market Data Dashboard, realtime financial app)",
+        "Led frontend, backend & database implementation",
+        "Implemented authentication framework",
+        "Enabled stakeholders to retrieve metrics in a fast webapp using cassandra and kdb"
+      ],
+      "stack": [
+        { "id": 1, "text": "Typescript", "color": "red", "image": "images/typescript.png" },
+        { "id": 2, "text": "Angular", "color": "red", "image": "images/angular.svg" },
+        { "id": 3, "text": "AWS", "color": "yellow", "image": "images/aws.svg" },
+        { "id": 4, "text": "Java", "color": "red", "image": "images/java.svg" },
+        { "id": 5, "text": "Cassandra", "color": "red", "image": "images/cassandra.svg" },
+        { "id": 6, "text": "Sybase", "color": "red", "image": "images/sybase.png" },
+        { "id": 7, "text": "KDB", "color": "red", "image": "images/kdb.png" },
+        { "id": 8, "text": "AG Grid", "color": "red", "image": "images/aggrid.png" }
+      ]
+    },
+    {
+      "type": "work_experience",
+      "role": "Ssr Javascript Developer",
+      "company": "Technisys SA",
+      "country": "images/ar.svg",
+      "company_link": "https://www.technisys.com/",
+      "started": "2015-05",
+      "ended": "2018-05",
+      "bullets": [
+        "Leading a team of 6, completed 4 apps for 4 different banks:",
+        "Produced the corporate home banking for Patagonia's Bank",
+        "Completed the ATM app for BROU Bank (Uruguay)",
+        "Contributed in the retail home banking for Falabella Chile Bank.",
+        "Achieved a mobile solution for Itaú Bank."
+      ],
+      "stack": [
+        { "id": 1, "text": "Javascript", "color": "red", "image": "images/javascript.svg" },
+        { "id": 2, "text": "Java", "color": "red", "image": "images/java.svg" },
+        { "id": 3, "text": "SQL", "color": "red", "image": "images/sql.png" },
+        { "id": 4, "text": "Microservices", "color": "red", "image": "images/microservices.png" },
+        { "id": 5, "text": "Team leading", "color": "red", "image": "images/teamleading1.png" },
+      ]
+    },
+    {
+      "type": "work_experience",
+      "role": "Junior/Ssr web developer",
+      "company": "Freelance",
+      "country": "images/panama.svg.png",
+      "company_link": "https://www.freelance.com/",
+      "started": "2012-11",
+      "ended": "2015-03",
+      "bullets": [
+        "Several small projects for retail shops and agencies in Panamá.",
+        "As a backend Java developer, my skills included AWT, CRUD apps, Hibernate and Spring framework. ",
+        "As a frontend Javascript developer, I specialized in HTML5, AngularJS, Bootstrap and jQuery.",
+        "Most important project, completed a new propietary software for an environmental consulting company in Panamá."
+      ],
+      "stack": [
+        { "id": 1, "text": "Python", "color": "red", "image": "images/python.svg" },
+        { "id": 2, "text": "Javascript", "color": "light_blue", "image": "images/javascript.svg" },
+        { "id": 3, "text": "Microservices", "color": "magenta", "image": "images/microservices.png" },
+        { "id": 4, "text": "AWS", "color": "blue", "image": "images/aws.svg" },
+        { "id": 5, "text": "Java", "color": "yellow", "image": "images/java.svg" },
+        { "id": 6, "text": "Kubernetes", "color": "blue", "image": "images/kubernetes.svg" },
+        { "id": 7, "text": "Kafka", "color": "blue", "image": "images/kafka.svg" },
+        { "id": 8, "text": "Cassandra", "color": "blue", "image": "images/cassandra.svg" },
+      ]
+    },
+    {
+      "type": "work_experience",
+      "role": "Java professor",
+      "company": "UTN college",
+      "country": "images/ar.svg",
+      "company_link": "http://www.olx.in",
+      "started": "2011-03",
+      "ended": "2012-12",
+      "bullets": [
+        "Teached step by step CRUD GUI zero to app. Stack: Java, Spring, AWT, Swing, Hibernate",
+        "OOP theory, class and instance, inheritance, encapsulation, abstractions, polymorphism."
+      ],
+      "stack": [
+        { "id": 1, "text": "Java", "color": "yellow", "image": "images/java.svg" },
+        { "id": 2, "text": "SQL", "color": "blue", "image": "images/sql.png" },
+      ],
+      "hidable": true
+    },
+    {
+      "type": "work_experience",
+      "role": "Environmental research scholarship at college level",
+      "company": "UTN college",
+      "country": "images/ar.svg",
+      "started": "2012-02",
+      "ended": "2012-12",
+      "bullets": [
+        "Collaborated in the creation of the theoretical framework. Admin. Internal website.",
+        "Landing page, CMS, blog, survey, automated mailing and achieved basic metrics."
+      ],
+      "stack": [
+        { "id": 1, "text": "Team lead", "color": "magenta", "image": "images/teamleading1.png" },
+        { "id": 2, "text": "agile", "color": "orange", "image": "images/agile.png" },
+      ],
+      "hidable": true
+    },
+    {
+      "type": "work_experience",
+      "role": "Unix Admin Jr.",
+      "company": "ELSERVER.COM",
+      "country": "images/ar.svg",
+      "company_link": "https://latincloud.com/",
+      "started": "2009-03",
+      "ended": "2012-06",
+      "bullets": [
+        "Contributed to successful implementation of security measures and tools, like Nagios and Cacti.",
+        "Produced scripts to automate repetitive tasks, which led to reduce server load",
+        "Usual tasks: Slow sql queries, vulnerability issues, load balancing, DNSs, redirects, hacking issues, cloud and modernization"
+      ],
+      "stack": [
+        { "id": 1, "text": "Unix", "color": "magenta", "image": "images/linux.jpeg" },
+        { "id": 2, "text": "Bash", "color": "orange", "image": "images/bash.png" },
+        { "id": 3, "text": "Python", "color": "orange", "image": "images/python.svg" },
+        { "id": 4, "text": "Javascript", "color": "orange", "image": "images/javascript.svg" },
+        { "id": 5, "text": "Apache", "color": "orange", "image": "images/apache.png" },
+        { "id": 6, "text": "Virtualization", "color": "orange", "image": "images/virtualmachine.svg" },
+      ],
+      "hidable": false
+    },
+    {
+      "type": "work_experience",
+      "role": "Containment agent",
+      "company": "La Caja",
+      "country": "images/ar.svg",
+      "company_link": "https://seguros.lacaja.com.ar/",
+      "started": "2008-01",
+      "ended": "2008-12",
+      "bullets": ["Designed, implemented and maintained hardware store accounting and inventory system"],
+      "stack": [
+        { "id": 1, "text": "Soft skills", "color": "magenta", "image": "images/softskills.png" },
+        { "id": 2, "text": "Communication", "color": "orange", "image": "images/communication.png" }
+      ],
+      "hidable": true
+    },
+    {
+      "type": "work_experience",
+      "role": "Backoffice technical support",
+      "company": "Cablevisión",
+      "country": "images/ar.svg",
+      "company_link": "https://clientes.cablevisionfibertel.com.ar/",
+      "started": "2005-01",
+      "ended": "2007-12",
+      "bullets": [
+        "TCP/IP. DHCP. ADSL. Wifi. Webmail.",
+        "Soft skills improved: communication, collaboration, team leading, customer service",
+        "Achievements:",
+        "-Created and maintained a documentation site to standarize 90% of most common issues with proper solutions across the Call Center.",
+        "-This impacted in a decreased amount of recalls related to technical issues, less spent solving time, and quicker onboarding of new employees."
+      ],
+      "stack": [
+        { "id": 1, "text": "Team lead", "color": "magenta", "image": "images/teamleading1.png" },
+        { "id": 2, "text": "Soft skills", "color": "magenta", "image": "images/softskills.png" },
+        { "id": 3, "text": "CCNA", "color": "yellow", "image": "images/cisco.jpeg" },
+        { "id": 4, "text": "Networks", "color": "yellow", "image": "images/network.png" },
+        { "id": 5, "text": "Security", "color": "yellow", "image": "images/security.jpeg" },
+        { "id": 6, "text": "HTML", "color": "yellow", "image": "images/html.png" }
+      ],
+      "hidable": true
+    }
+  ],
+  "education": [
+    {
+      "type": "education",
+      "name": "Superior Technical Institute (IFTS)",
+      "short_description": "3-year Information Systems Analyst degree",
+      "short_name": "<a href='https://www.ifts18.edu.ar/'>IFTS</a>",
+      "company_link": "https://www.ifts18.edu.ar/",
+      "title": "Information Systems Analyst degree (3 years)",
+      "degree_type": "",
+      "bullets": [
+        "3-year Information Systems Analyst degree"
+      ]
+    },
+    {
+      "type": "education",
+      "name": "UTN",
+      "short_description": "6-year Information Systems Engineering degree (<a href='https://en.wikipedia.org/wiki/Engineer&apos;s_degree#Latin_America'>equivalent to an Engineer degree</a>)",
+      "short_name": "<a href='https://www.utn.edu.ar/'>UTN</a>",
+      "company_link": "https://www.utn.edu.ar/",
+      "title": "Information Systems Analyst degree (6 years)",
+      "degree_type": "",
+      "date_range": "2010-2016",
+      "bullets": [
+        "5-year Information Systems Engineering degree (<a href='https://en.wikipedia.org/wiki/Engineer&apos;s_degree#Latin_America'>equivalent to Engineer degree</a>)"
+      ]
+    }
+  ],
+  "finalMessage": ""
+}
